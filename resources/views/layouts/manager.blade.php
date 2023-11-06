067209723e5f9637c9c4c940ff0f2d3b18d78671
TruongQDat <truongquocdat12cb2@gmail.com>
2023-11-07 01:37:20 +0700

    update

--- resources/views/layouts/manager.blade.php
+++ resources/views/layouts/manager.blade.php
@@ -23,8 +23,7 @@ https://cdn.jsdelivr.net/npm/sweetalert2@11.9.0/dist/sweetalert2.min.css
 " rel="stylesheet">
     <script src="https://cdn.tiny.cloud/1/cbq48gtdz0pfdbuzyype3yu466uymwxiwm110gup8nlexzj2/tinymce/6/tinymce.min.js"
         referrerpolicy="origin"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
